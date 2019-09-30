d0541865039e134ba3dc745d856a6aa721f72794
KhaNH <v.khanh3@vinid.net>
2019-09-30 15:12:41 +0700

    [Tulsi] Fix Unit Tests missing framework swift includes + Change clean to clean --expunge

--- src/TulsiGenerator/PBXTargetGenerator.swift
+++ src/TulsiGenerator/PBXTargetGenerator.swift
@@ -761,17 +761,6 @@ final class PBXTargetGenerator: PBXTargetGeneratorProtocol {
                                                         buildToolPath: "\(scriptPath)",
                                                         buildArguments: buildArgs,
                                                         buildWorkingDirectory: workingDirectory)
-
-    for target: PBXTarget in project.allTargets {
-      if target === bazelCleanScriptTarget {
-        continue
-      }
-
-      target.createDependencyOn(bazelCleanScriptTarget!,
-                                proxyType: PBXContainerItemProxy.ProxyType.targetReference,
-                                inProject: project,
-                                first: true)
-    }
   }
 
   func generateTopLevelBuildConfigurations(_ buildSettingOverrides: [String: String] = [:]) {
@@ -1297,6 +1286,14 @@ final class PBXTargetGenerator: PBXTargetGeneratorProtocol {
       let fullPath = module.fullPath as NSString
       let includePath = fullPath.deletingLastPathComponent
       swiftIncludes.add("$(\(PBXTargetGenerator.BazelWorkspaceSymlinkVarName))/\(includePath)")
+    }
+    
+    for module in ruleEntry.objCModuleMaps {
+      let fullPath = module.fullPath as NSString
+      let includePath = fullPath.deletingLastPathComponent
+      if includePath.contains(".framework") {
+        swiftIncludes.add("$(\(PBXTargetGenerator.BazelWorkspaceSymlinkVarName))/\(includePath)")
+      }
     }
   }
 
