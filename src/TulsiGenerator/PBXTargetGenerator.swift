b75793e52609de82071f3bbe2e178cc779f9837e
KhaNH <v.khanh3@vinid.net>
2019-10-20 16:35:05 +0700

    [Tulsi] Don't call bazel clean for every build to save time

--- src/TulsiGenerator/PBXTargetGenerator.swift
+++ src/TulsiGenerator/PBXTargetGenerator.swift
@@ -1603,12 +1603,12 @@ final class PBXTargetGenerator: PBXTargetGeneratorProtocol {
       target.buildPhases.append(buildPhase)
     }
 
-    if let legacyTarget = bazelCleanScriptTarget {
-      target.createDependencyOn(legacyTarget,
-                                proxyType: PBXContainerItemProxy.ProxyType.targetReference,
-                                inProject: project,
-                                first: true)
-    }
+    // if let legacyTarget = bazelCleanScriptTarget {
+    //   target.createDependencyOn(legacyTarget,
+    //                             proxyType: PBXContainerItemProxy.ProxyType.targetReference,
+    //                             inProject: project,
+    //                             first: true)
+    // }
 
     return target
   }
