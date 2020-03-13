0f60ce715745ae9ea6066d4b28ca30a60c7d9a15
KhaNH <v.khanh3@vinid.net>
2020-03-13 14:41:29 +0700

    Add code coverage support and generate HTML report

--- src/TulsiGenerator/XcodeProjectGenerator.swift
+++ src/TulsiGenerator/XcodeProjectGenerator.swift
@@ -731,6 +731,8 @@ final class XcodeProjectGenerator {
     let userSchemesURL = projectURL.appendingPathComponent(userSchemeSubpath)
     guard createDirectory(userSchemesURL) else { return }
 
+    let features = BazelBuildSettingsFeatures.enabledFeatures(options: config.options)
+    
     func updateManagementDictionary(
       _ dictionary: inout [String: Any],
       schemeName: String,
@@ -896,7 +898,8 @@ final class XcodeProjectGenerator {
                                  environmentVariables: schemeEnvVars,
                                  preActionScripts:preActionScripts(for: entry),
                                  postActionScripts:postActionScripts(for: entry),
-                                 localizedMessageLogger: localizedMessageLogger)
+                                 localizedMessageLogger: localizedMessageLogger,
+                                 codeCoverageEnabled: features.contains(.HTMLCodeCoverage))
         let xmlDocument = scheme.toXML()
 
         filename += target.name + ".xcscheme"
@@ -969,7 +972,9 @@ final class XcodeProjectGenerator {
       let additionalBuildTargets = indexerTargets.map() {
         ($0, projectBundleName, XcodeScheme.makeBuildActionEntryAttributes())
       }
-
+      
+      let features = BazelBuildSettingsFeatures.enabledFeatures(options: config.options)
+      
       let scheme = XcodeScheme(target: nil,
                                project: info.project,
                                projectBundleName: projectBundleName,
@@ -977,7 +982,8 @@ final class XcodeProjectGenerator {
                                additionalBuildTargets: additionalBuildTargets,
                                preActionScripts: [:],
                                postActionScripts: [:],
-                               localizedMessageLogger: localizedMessageLogger)
+                               localizedMessageLogger: localizedMessageLogger,
+                               codeCoverageEnabled: features.contains(.HTMLCodeCoverage))
       let xmlDocument = scheme.toXML()
 
       let data = xmlDocument.xmlData(options: XMLNode.Options.nodePrettyPrint)
@@ -998,6 +1004,8 @@ final class XcodeProjectGenerator {
 
       let filename = suiteName + "_Suite.xcscheme"
 
+      let features = BazelBuildSettingsFeatures.enabledFeatures(options: config.options)
+      
       let url = xcschemesURL.appendingPathComponent(filename)
       let scheme = XcodeScheme(target: extractedHostTarget,
                                project: info.project,
@@ -1010,7 +1018,8 @@ final class XcodeProjectGenerator {
                                environmentVariables: environmentVariables(for: suite),
                                preActionScripts: preActionScripts(for: suite),
                                postActionScripts:postActionScripts(for: suite),
-                               localizedMessageLogger: localizedMessageLogger)
+                               localizedMessageLogger: localizedMessageLogger,
+                               codeCoverageEnabled: features.contains(.HTMLCodeCoverage))
       let xmlDocument = scheme.toXML()
 
 
