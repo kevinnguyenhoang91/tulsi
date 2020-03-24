48dad1fab111fd9b460a2bcf92d80dc0450e6780
KhaNH <v.khanh3@vinid.net>
2020-03-24 12:08:53 +0700

    Better managing code coverage schemes

--- src/TulsiGenerator/PBXTargetGenerator.swift
+++ src/TulsiGenerator/PBXTargetGenerator.swift
@@ -951,6 +951,8 @@ final class PBXTargetGenerator: PBXTargetGeneratorProtocol {
   func generateCodeCoverageTargetsForRuleEntries(_ ruleEntries: Set<RuleEntry>,
                                                  codeCoverageReportScriptPath: String,
                                                  workingDirectory: String) throws {
+    guard enabledFeatures().contains(.HTMLCodeCoverage) else { return }
+    
     let namedRuleEntries = generateUniqueNamesForRuleEntries(ruleEntries)
 
     let progressNotifier = ProgressNotifier(name: GeneratingCodeCoverageTargets,
@@ -966,13 +968,13 @@ final class PBXTargetGenerator: PBXTargetGeneratorProtocol {
         var coverageFilterRegex = ""
         if let regexVar = coverageRegexes[targetName] {
           coverageFilterRegex = regexVar
+          let coverageIgnoreRegex = codeCoverageIgnoreFilterRegexes(for: entry)
+          let startupOptions = [coverageFilterRegex, coverageIgnoreRegex, targetName]
+          generateCodeCoverageReportTarget(name,
+                                          codeCoverageReportScriptPath, 
+                                          workingDirectory: workingDirectory,
+                                          startupOptions: startupOptions)
         }
-        let coverageIgnoreRegex = codeCoverageIgnoreFilterRegexes(for: entry)
-        let startupOptions = [coverageFilterRegex, coverageIgnoreRegex, targetName]
-        generateCodeCoverageReportTarget(name,
-                                         codeCoverageReportScriptPath, 
-                                         workingDirectory: workingDirectory,
-                                         startupOptions: startupOptions)
       }
     }
   }
@@ -1299,11 +1301,6 @@ final class PBXTargetGenerator: PBXTargetGeneratorProtocol {
                                                      withPerFileSettings: nonARCSettings)
       target.buildPhases.append(buildPhase)
     }
-    
-    // if enabledFeatures().contains(.HTMLCodeCoverage) {
-    //   let testBuildPhase = createGenerateHTMLCodeCoverageTestBuildPhase(ruleEntry)
-    //   target.buildPhases.append(testBuildPhase)
-    // }
   }
 
   /// Adds includes paths from the RuleEntry to the given NSSet.
