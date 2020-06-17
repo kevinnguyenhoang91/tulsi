5ab820bc9982d57942d7f50d79001b9b55684d57
KhaNH <v.khanh3@vinid.net>
2020-06-17 09:19:49 +0700

    Dont clean expunge cause it is not neccessary + Change condition to filter xcode sources

--- src/TulsiGenerator/Scripts/bazel_clean.sh
+++ src/TulsiGenerator/Scripts/bazel_clean.sh
@@ -25,9 +25,9 @@ readonly bazel_executable="$1"; shift
 readonly bazel_bin_dir="$1"; shift
 
 if [ -z $# ]; then
-  readonly arguments=(clean "--expunge")
+  readonly arguments=(clean)
 else
-  readonly arguments=("$@" clean "--expunge")
+  readonly arguments=("$@" clean)
 fi
 
 if [[ "${ACTION}" != "clean" ]]; then
