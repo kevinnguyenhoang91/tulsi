51ab19a14f66b833c33133d7fb63f8458772cb2b
KhaNH <v.khanh3@vinid.net>
2020-06-17 09:19:49 +0700

    Enhanced Xcode indexing for local_repository by follow symlinked sources

--- src/TulsiGenerator/BazelXcodeProjectPatcher.swift
+++ src/TulsiGenerator/BazelXcodeProjectPatcher.swift
@@ -96,6 +96,6 @@ final class BazelXcodeProjectPatcher {
   // Handles patching any groups that were generated under Bazel's magical "external" container to
   // proper filesystem references. This should be called after patchBazelRelativeReferences.
   func patchExternalRepositoryReferences(_ xcodeProject: PBXProject, _ bazelExecRoot: String) {
-    fileReferencePatcher.patchExternalRepositoryReferencesAlternative(xcodeProject, bazelExecRoot)
+    fileReferencePatcher.patchExternalRepositoryReferencesVersioning(xcodeProject, bazelExecRoot)
   }
 }
